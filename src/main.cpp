c15c96c29363dfbd99b5b576a380367e019343fa
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2014-03-04 03:09:47 -0500

    Skeleton implementation of main() with SDL.
    Compiles directly on Win32 and Linux.

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,17 @@
  */
 #include <iostream>
 #include <iomanip>
+#include <SDL2/SDL.h>
 
 int main(int argc, char ** argv)
 {
+
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+  {
+    std::cout << "SDL_Init Error: " << SDL_GetError() << "\n";
+    return 1;
+  }
+  SDL_Quit();
 
   return 0;
 }
