e8ca94b97bfb8995ba783ca9dc26686141a64bfb
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2014-03-10 01:24:08 -0400

    First Implementation of main game loop.

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,35 @@
  * Author: Ryan - David Reyes
  * Description:
  */
+//standard includes
 #include <iostream>
 #include <iomanip>
+
+//external library includes
 #include <SDL2/SDL.h>
+
+//program includes
+#include <gameengine.h>
 
 int main(int argc, char ** argv)
 {
+  int return_value = 0;
 
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+  //Create the game context
+  GameEngine game;
+
+  //start the game
+  return_value = game.Init();
+
+  //main loop
+  while (game.IsRunning())
   {
-    std::cout << "SDL_Init Error: " << SDL_GetError() << "\n";
-    return 1;
+    game.HandleEvents();
+    game.Update();
+    game.Render();
   }
-  SDL_Quit();
 
-  return 0;
+  //delete the game
+  game.Cleanup();
+  return return_value;
 }
