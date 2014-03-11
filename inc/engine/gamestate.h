3262c628074491d8996cad5c6241c6bc1b2d5fde
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2014-03-11 00:21:12 -0400

    Fixed bug where gamestate ctor was private.
    It is now protected.

--- inc/engine/gamestate.h
+++ inc/engine/gamestate.h
@@ -3,6 +3,24 @@
 
 #include <engine/gameengine.h>
 
+/*
+ * Description:
+ *  This class defines the interface of the game state.
+ *  All main game states (main menu, pause screen, in game, victory, etc)
+ *  shall derive from this class.
+ *
+ * Functions:
+ *  Init:     Initializes needed resources when entering this state.
+ *  Cleanup:  Destroys allocated resources when exiting this state.
+ *  Pause:    Deals with state interruption, i.e. if the state is interrupted
+ *    but will be resumed later.
+ *  Resume:   The state continues if it was Pause()d.
+ *  HandleEvents: This state's own interpretation of keyboard/mouse events.
+ *  Update:       How this state updates it's own internal logic (menu vs game).
+ *  Render:       What this state will draw to the screen.
+ */
+
+
 class GameState
 {
   public:
@@ -18,7 +36,7 @@ class GameState
 
     void ChangeState(GameEngine *game, GameState *state)
     {game->ChangeState(state);}
-  private:
+  protected:
     GameState() {}
 };
 
