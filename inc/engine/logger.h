3bd4afa5d7ba6aea3335ad6759037e06e58fcfa6
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2014-03-12 01:42:28 -0400

    Skeleton implementation of logger.h.
    This logger will be a simple but robust logging implementation.
    It will allow for different levels of severity logging, and be thread
    safe.

---
+++ inc/engine/logger.h
@@ -0,0 +1,24 @@
+#ifndef LOGGER_H_
+#define LOGGER_H_
+
+namespace engine
+{
+
+enum SeverityLevel
+{
+  LOG_INFO,
+  LOG_DEBUG,
+  LOG_ERROR,
+  LOG_FATAL,
+}
+
+class Logger
+{
+  public:
+
+  private:
+
+};
+}
+#endif
+
